85b39bf33b444f7366e7db4785cbc43448f628d1
eutropius225 <mega9yo@gmail.com>
2020-09-11 13:58:07 +0100

    Log calls to 'sicp.

--- chapters/sicp-eval.rkt
+++ chapters/sicp-eval.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 (require racket/sandbox
-         scribble/example)
+         scribble/example
+         (for-syntax racket/base))
 (provide sicp
          sicpnl
          sicp-evaluator)
@@ -12,9 +13,16 @@
                  [sandbox-propagate-exceptions #f])
     (make-evaluator 'sicp)))
 
-(define-syntax-rule
-  (sicp exp ...)
-  (examples #:eval sicp-evaluator exp ...))
+(define-syntax
+  (sicp arg)
+  (display "Evaluating Scheme at ")
+  (display (syntax-line arg))
+  (display ":")
+  (display (syntax-column arg))
+  (display ":")
+  (display (syntax-source arg))
+  (newline)
+  #'(examples #:eval sicp-evaluator ,arg))
 
 (define-syntax-rule
   (sicpnl exp ...)
